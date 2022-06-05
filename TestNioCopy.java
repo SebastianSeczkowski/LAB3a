1617ea1bf45796bab9139bd79abe90f7b72520fd
SebastianSeczkowski <37137@studentwsei.pl>
2022-06-06 01:04:22 +0200

    Realizacja punktu 9

--- TestNioCopy.java
+++ TestNioCopy.java
@@ -1,5 +1,6 @@
 package pl.lublin.wsei.java.cwiczenia.console;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -10,10 +11,15 @@ public class TestNioCopy {
     public static void main(String[] args) throws IOException {
         // long startTime = System.currentTimeMillis();
         long startTime = System.nanoTime(); // dokladniejszy pomiar
-        Path oryginal = Paths.get("sample_1920×1280.tiff");
-        Path kopia = Paths.get("img_copy2.tiff");
-        Files.copy(oryginal, kopia, StandardCopyOption.REPLACE_EXISTING);
-        //  System.out.println("Czas kopiowania: " + (System.currentTimeMillis() - startTime)+" ms");
+       //  Path oryginal = Paths.get("sample_1920×1280.tiff");
+      //  Path kopia = Paths.get("img_copy2.tiff");
+      //  Files.copy(oryginal, kopia, StandardCopyOption.REPLACE_EXISTING);
+
+        File oryginal = new File("sample_1920×1280.tiff");
+        File kopia = new File("img_copy2.tiff");
+        Files.copy(oryginal.toPath(), kopia.toPath());
+        
+        // System.out.println("Czas kopiowania: " + (System.currentTimeMillis() - startTime)+" ms");
         System.out.println("Czas kopiowania: " + (float)(System.nanoTime() - startTime)/1000000+" ms");
     }
 }
